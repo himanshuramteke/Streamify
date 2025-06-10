951b82a2d761311b2c15f0b0d6609e4a498a424a
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-06-10 17:51:14 +0530

    Added onboarding route and protectRoute middleware

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,19 @@
 import express from "express";
-import { loginController, logoutController, signupController } from "../controllers/auth.controller.js";
+import { loginController, logoutController, onboardController, signupController } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/login", loginController);
 router.post("/logout", logoutController);
 router.post("/signup", signupController);
+
+router.post("/onboarding", protectRoute, onboardController);
+
+//check if user is logged in
+router.get("/me", protectRoute, (req, res) => {
+    res.status(200).json({ success: true, user: req.user });
+});
 
 
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import "dotenv/config";
+import cookieParser from 'cookie-parser';
+
 import authRoutes from "./routes/auth.route.js"
 import { connectDB } from './config/dbConfig.js';
+
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
