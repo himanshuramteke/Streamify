95e48bcebd46d42b7386fe0f171f841f5953e46b
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-06-10 09:54:59 +0530

    Added loginController and logoutController logic

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -45,7 +45,7 @@ export async function signupController(req, res) {
             secure: process.env.NODE_ENV === "production",
         });
 
-        res.status(200).json({ success: true, user: newUser });
+        res.status(201).json({ success: true, user: newUser });
     } catch (error) {
         console.log("Error in signup controller", error);
         res.status(500).json({ message: "Internal server error" });
@@ -53,9 +53,37 @@ export async function signupController(req, res) {
 }
 
 export async function loginController(req, res) {
-    res.send("login route");
+    try {
+        const {email, password} = req.body;
+
+        if(!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+        const user = await User.findOne({ email });
+        if(!user) return res.status(401).json({ message: "Invalid email or password" });
+
+        const isPasswordCorrect = await user.matchPassword(password);
+        if(!isPasswordCorrect) return res.status(401).json({ message: "Invalid email or password" });
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
+            expiresIn: "7d"
+        });
+
+        res.cookie("jwt", token, {
+            maxAge: 7 * 24 * 60 * 1000,
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV === "production",
+        });
+
+        res.status(200).json({ success: true, user})
+    } catch (error) {
+        console.log("Error in login controller", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
 }
 
 export function logoutController(req, res) {
-    res.send("logout route");
+    res.clearCookie("jwt");
+    res.status(200).json({ success: true, message: "Logout successfull" });
 }
